7ba65459e0a179e63c17c941f10bc88304e2bec2
Spencer <sthompson.dev@outlook.com>
2025-06-09 11:31:58 -0600

    updates to dotfiles

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -4,10 +4,7 @@ function update
     sudo reflector --age 2 --connection-timeout 1 --download-timeout 1 --country US \
         --fastest 12 --latest 10 --number 12 --save /etc/pacman.d/mirrorlist
 
-    echo -e "\e[1;35mUpdating Pacman Packages\e[0m"
-    sudo pacman -Syu --noconfirm
-
-    echo -e "\e[1;35mUpdating AUR Packages\e[0m"
+    echo -e "\e[1;35mUpdating Packages\e[0m"
     paru -Syu --noconfirm
 
     echo -e "\e[1;35mUpdating Completions\e[0m"
