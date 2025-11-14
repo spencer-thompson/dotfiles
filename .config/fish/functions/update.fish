af211fb138b2504929042146ffdcb5bdd1b3c8dd
Spencer <sthompson.dev@outlook.com>
2025-11-13 23:35:07 -0700

    better game performance, zathura fixes

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,9 +1,5 @@
 # UPDATE
 function update
-    echo -e "\e[1;35mUpdating Mirror List\e[0m"
-    sudo reflector --age 2 --connection-timeout 1 --download-timeout 1 --country US \
-        --fastest 12 --latest 10 --number 12 --save /etc/pacman.d/mirrorlist
-
     echo -e "\e[1;35mUpdating Packages\e[0m"
     paru -Syu
 
